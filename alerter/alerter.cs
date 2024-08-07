c563e101cdef2392f0591c7e8396249fb4991b5c
prabhatkapse <prabhatkapse@gmail.com>
2024-08-07 12:56:12 +0530

    Update alerter.cs

--- alerter/alerter.cs
+++ alerter/alerter.cs
@@ -1,16 +1,19 @@
 using System;
+using System.Diagnostics;
 
 namespace AlerterSpace {
     class Alerter {
         static int alertFailureCount = 0;
-        static int networkAlertStub(float celcius) {
+        static int networkAlertStub(float celcius)
+        {
             Console.WriteLine("ALERT: Temperature is {0} celcius", celcius);
             // Return 200 for ok
             // Return 500 for not-ok
             // stub always succeeds and returns 200
             return 200;
         }
-        static void alertInCelcius(float farenheit) {
+        static void alertInCelcius(float farenheit) 
+        {
             float celcius = (farenheit - 32) * 5 / 9;
             int returnCode = networkAlertStub(celcius);
             if (returnCode != 200) {
@@ -21,9 +24,14 @@ namespace AlerterSpace {
                 alertFailureCount += 0;
             }
         }
-        static void Main(string[] args) {
+        static void Main(string[] args) 
+        {
             alertInCelcius(400.5f);
+            Debug.Assert(alertFailureCount == 1);
+
             alertInCelcius(303.6f);
+            Debug.Assert(alertFailureCount == 2);
+
             Console.WriteLine("{0} alerts failed.", alertFailureCount);
             Console.WriteLine("All is well (maybe!)\n");
         }
