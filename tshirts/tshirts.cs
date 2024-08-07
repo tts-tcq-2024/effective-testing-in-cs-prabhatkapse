66aec59a1b15d8d7be9e99d217052ce79d2bcd62
prabhatkapse <prabhatkapse@gmail.com>
2024-08-07 12:38:54 +0530

    Update tshirts.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -17,6 +17,7 @@ namespace TshirtSpace {
             Debug.Assert(Size(40) == "M");
             Debug.Assert(Size(43) == "L");
             Debug.Assert(Size(38) == "M");
+            Debug.Assert(Size(38) == "S");
             Console.WriteLine( "All is well (maybe!)");
         }
     }
