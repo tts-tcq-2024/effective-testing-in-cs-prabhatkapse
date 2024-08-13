6b39742bdad533130f4bd96b979395fd8eec3055
prabhatkapse <prabhatkapse@gmail.com>
2024-08-13 15:44:11 +0530

    Update misaligned.cs

--- misaligned/misaligned.cs
+++ misaligned/misaligned.cs
@@ -2,9 +2,23 @@ using System;
 using System.Diagnostics;
 
 namespace MisalignedSpace {
-    class Misaligned {
+    class Misaligned 
+    {
         static void Main(string[] args) {
-            var colorMap = ColorCode.GenerateColorMap();
+
+            ColorCode colorCode;
+
+            colorCode = new ColorCode(new PrintOnConsol());
+
+            var result = colorCode.PrintColorMap();
+
+            Debug.Assert(result == 25);
+
+            var MopPrintOnConsol = new MopPrintOnConsol();
+            colorCode = new ColorCode(MopPrintOnConsol);
+            colorCode.PrintColorMap();
+            var colorMap = MopPrintOnConsol._colorMap;
+
             Debug.Assert(colorMap[0] ==  "1 | White | Blue");
             Debug.Assert(colorMap[1] ==  "2 | White | Orange");
             Debug.Assert(colorMap[8] ==  "9 | Red | Brown");
@@ -17,8 +31,6 @@ namespace MisalignedSpace {
             Debug.Assert(colorMap[8] ==  "09 | Red    | Brown");
             Debug.Assert(colorMap[17] == "18 | Yellow | Green");
             Debug.Assert(colorMap[22] == "23 | Violet | Green");
-
-            PrintStringArray.PrintColorMap(colorMap);
 
             Console.WriteLine("All is well (maybe!)");
         }
